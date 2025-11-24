54aa65ea0ec5271cef805398809b13eed83e57d9
Khun <lykhun@gmail.com>
2025-11-24 19:50:51 +0100

    add test

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ import Express from 'express';
 const app = Express();
 
 app.get('/', (_, res) => {
-    res.json({ value: process.env.secret })
+    res.json({ value: process.env.secret, value2: f() })
 });
+
+export function f() {
+    return 42;
+}
 
 app.listen(3000);
---
+++ src/app.test.mjs
@@ -0,0 +1,5 @@
+import { f } from "./app"
+
+test('Test pour Aurelien', () => {
+    expect(f()).toBe(42);
+});
